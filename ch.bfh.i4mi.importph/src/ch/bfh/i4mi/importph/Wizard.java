7d7a283418477cb353a60300d0aa213ec3c814db
dengyihan <dengyihan@gmail.com>
2017-03-01 14:38:59 +0100

    add import plugin
    abcdefg

---
+++ ch.bfh.i4mi.importph/src/ch/bfh/i4mi/importph/Wizard.java
@@ -0,0 +1,11 @@
+package ch.bfh.i4mi.importph;
+
+public class Wizard extends org.eclipse.jface.wizard.Wizard {
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
